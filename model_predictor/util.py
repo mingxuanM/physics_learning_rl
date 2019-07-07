f0a76f327d3c5e269cc0dc9dec9609224a3d9ba1
Mingxuan_Mei <s1871044@sms.ed.ac.uk>
2019-07-07 14:46:49 +0100

    Merge baseline generation to master

--- model_predictor/util.py
+++ model_predictor/util.py
@@ -1,7 +1,10 @@
 import json
 import numpy as np
-# Calculate mean standard deviation of x,y,xv,yv in trails in original training set
 
+input_frame = 5
+n_state =16
+
+# Calculate mean standard deviation of x,y,xv,yv in trails in original training set
 def std_div():
     with open('lsclips.json') as lsclips_json:  
         lsclips = json.load(lsclips_json)
@@ -17,6 +20,84 @@ def std_div():
             std_yv.append(np.std(lsclips[p][t]['o1.vy'] + lsclips[p][t]['o2.vy'] + lsclips[p][t]['o3.vy'] + lsclips[p][t]['o4.vy']))
     return np.mean(std_x), np.mean(std_xv), np.mean(std_y), np.mean(std_yv)
 # mean standard deviation for x is: 1.6973236662957578
-# mean standard deviation for xv is: 1.7830307647729327
 # mean standard deviation for y is: 1.0517453222060384
+# mean standard deviation for xv is: 1.7830307647729327
 # mean standard deviation for yv is: 1.81123320699984
+# Sum to 6.3432
+
+
+# Baseline cases
+# 1. Worst case
+# Sample same time step frames from a random trail as prediction
+# Worst case baseline loss: [5.73467882 2.21698449 6.72088708 6.80948858]
+def Worst_case_baseline(data):
+    print('Worst_case_baseline started...')
+    epoch_loss = np.zeros((len(data), int(n_state/4)))
+    for s_idx, sequence in enumerate(data):
+        batch_num = len(sequence) // (input_frame+1)
+        sequence_loss = np.zeros((batch_num, int(n_state/4)))
+        for n in range(batch_num):
+            start = n*(input_frame+1)
+            end = start+5
+            label = sequence[end,-n_state:]
+            prediction_trail = data[np.random.random_integers(len(data))-1]
+            if len(prediction_trail) <= end:
+                prediction = prediction_trail[-1,-n_state:]
+            else:
+                prediction = prediction_trail[end,-n_state:]
+            batch_loss_ = np.square(np.subtract(label, prediction))
+            sequence_loss[n] = np.array([
+                        np.mean([batch_loss_[0],batch_loss_[4],batch_loss_[8],batch_loss_[12]]), # x
+                        np.mean([batch_loss_[1],batch_loss_[5],batch_loss_[9],batch_loss_[13]]), # y
+                        np.mean([batch_loss_[2],batch_loss_[6],batch_loss_[10],batch_loss_[14]]), # vx
+                        np.mean([batch_loss_[3],batch_loss_[7],batch_loss_[11],batch_loss_[15]])  # vy
+                    ])
+        epoch_loss[s_idx] = np.mean(sequence_loss, axis=0)
+    final_loss = np.mean(epoch_loss, axis=0)
+    print('Worst case baseline loss: '+ str(final_loss))
+
+ 
+
+# 2. Infinite inertia case
+# Use last frame as prediction
+# Infinite inertia baseline loss: [0.00092996 0.00093225 0.31726211 0.42942083]
+def Infinite_inertia_case_baseline(data):
+    print('Infinite_inertia_case_baseline started...')
+    # input_frame = 5
+    # n_state =16
+    epoch_loss = np.zeros((len(data), int(n_state/4)))
+    for s_idx, sequence in enumerate(data):
+        batch_num = len(sequence) // (input_frame+1)
+        sequence_loss = np.zeros((batch_num, int(n_state/4)))
+        for n in range(batch_num):
+            start = n*(input_frame+1)
+            end = start+5
+            label = sequence[end,-n_state:]
+            prediction = sequence[end-1,-n_state:]
+            batch_loss_ = np.square(np.subtract(label, prediction))
+            sequence_loss[n] = np.array([
+                        np.mean([batch_loss_[0],batch_loss_[4],batch_loss_[8],batch_loss_[12]]), # x
+                        np.mean([batch_loss_[1],batch_loss_[5],batch_loss_[9],batch_loss_[13]]), # y
+                        np.mean([batch_loss_[2],batch_loss_[6],batch_loss_[10],batch_loss_[14]]), # vx
+                        np.mean([batch_loss_[3],batch_loss_[7],batch_loss_[11],batch_loss_[15]])  # vy
+                    ])
+        epoch_loss[s_idx] = np.mean(sequence_loss, axis=0)
+    final_loss = np.mean(epoch_loss, axis=0)
+    print('Infinite inertia baseline loss: '+ str(final_loss))
+
+
+if __name__ == '__main__':
+    # load training data
+    with open('data/trails_data.json') as json_file:  
+        data = json.load(json_file)
+        # remove two outliers from training_data [685] & [692]
+        data = np.array(data[:685]+data[686:692]+data[693:])
+    print('training data loaded')
+    Infinite_inertia_case_baseline(data)
+    # Infinite inertia baseline loss: [0.00092996 0.00093225 0.31726211 0.42942083]
+    Worst_case_baseline(data)
+    # Worst case baseline loss: [5.73467882 2.21698449 6.72088708 6.80948858]
+
+    # 40 epochs reduced input model: [0.00147525 0.0013423  0.33779012 0.29830249]
+    # 40 epochs full input model: [0.00370258 0.00293012 0.24513803 0.25428428]
+
